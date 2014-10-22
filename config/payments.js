8f6ee15a174bcefa86906e94f2e47e67b0ea78a8
adam souzis <adam@souzis.com>
2014-10-22 16:16:48 -0700

    make app somewhat configurable

---
+++ config/payments.js
@@ -0,0 +1,9 @@
+module.exports = {
+apiKey: 'ak-test-eyoGATiAg6YE5thvhSiWIi7NE0zg0l0U', // test key
+password: '',
+baseUrl: "https://api.balancedpayments.com",
+defaultStatementText: "One Commons",
+defaultDescription: "User subscription payment",
+minAmountCents: 100,  // $1
+maxAmountCents: 1500000 // bp max is $15,000
+}
