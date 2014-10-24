0c175043cac7a18f37fea7080449fc58af70c8d9
adam souzis <adam@souzis.com>
2014-10-24 13:38:34 -0700

    bug fix

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ configure app, update and expose schemas
 add routes
 */
 var path = require('path');
+var base = require('base');
 
 module.exports = function install(app) {
   var models = require('./models');
   app.updateNamedRoutes(require('./routes'));
   //XXX hack, need better way to load config
-  var config = brequire('./lib/config')(path.resolve(app.rootDir), __dirname)('payments');
+  var config = base.brequire('./lib/config')(path.resolve(app.rootDir), __dirname)('payments');
   require('./lib/oc-balanced').setConfig(config);
 }
